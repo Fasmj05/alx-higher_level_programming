c78a0567d2e2a94baf18f985040f4f7bf25b8a27
Fasmj05 <mbanefofrancis@gmail.com>
2023-01-11 00:40:42 +0100

    hot fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a file-writing function."""
+"""Defines a class Student."""
 
 
-def write_file(filename="", text=""):
-    """Writes a string to a UTF8 text file
-    """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__)
